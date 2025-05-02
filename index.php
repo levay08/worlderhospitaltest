f6f74e97433d0bb9293b98dbdc457061856f45ef
VAY <dorayakicool@gmail.com>
2025-05-02 16:31:59 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Worlder Hospital</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Worlder Hospital</h1>
+        <p>
+            <a href="register.php">Register as Patient</a> |
+            <a href="login.php">Login</a> |
+            <a href="doctor_list.php">View Doctors</a>
+        </p>
+    </div>
+</body>
+</html>
